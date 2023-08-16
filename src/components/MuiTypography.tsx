e409a577ab7e22d0bbbb0a703f7003c5553a15e7
yoex <ytentertainment94@gmail.com>
2023-08-16 16:54:11 -0400

    first commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+    return (<div>
+        <Typography variant="h1">h1 Heading</Typography>
+        <Typography variant="h2">h2 Heading</Typography>
+        <Typography variant="h3">h3 Heading</Typography>
+
+    </div>)
+}
