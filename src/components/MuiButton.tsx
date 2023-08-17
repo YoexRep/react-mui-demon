ce43555acd489ff2623faae0b28b9428cf742e32
yoex <ytentertainmnet94@gmail.com>
2023-08-17 15:06:39 -0400

    MuiButtons

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,11 +1,68 @@
-import { Button } from "@mui/material";
+import { Stack, Button, IconButton } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 export const MuiButton = () => {
-    return (<div>
-
-        <Button variant="text">texto</Button>
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          texto
+        </Button>
         <Button variant="contained">Contained</Button>
         <Button variant="outlined">Outlined</Button>
+      </Stack>
 
-    </div>)
-}
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack display='block' spacing={2} direction='row'>
+      <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+
+      
+
+      </Stack>
+
+      <Stack  spacing={2} direction='row'>
+ 
+
+        <Button variant="contained" startIcon={<SendIcon/> } disableRipple onClick={()=> alert('Hola mundo')}>SEND</Button>
+        <Button variant="contained" endIcon={<SendIcon/>} disableElevation>SEND</Button>
+
+        <IconButton aria-label="send" color='success' size='large'>
+            <SendIcon></SendIcon>
+
+        </IconButton>
+
+      </Stack>
+    </Stack>
+  );
+};
