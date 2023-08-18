7a813b3e00b47cd4465e143cecdfdf00de2a0cff
yoex <ytentertainmnet94@gmail.com>
2023-08-18 09:18:49 -0400

    group y toggleButton Agregado

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,9 +1,27 @@
-import { Stack, Button, IconButton } from "@mui/material";
+import { Stack, Button, IconButton, ButtonGroup, ToggleButtonGroup, ToggleButton} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderLinedIcon from '@mui/icons-material/FormatUnderlined';
+
+import { useState } from "react";
+import { format } from "path";
+
 export const MuiButton = () => {
+
+  const [formats, setFormats] = useState<string[]>([])
+
+  console.log({formats,} )
+
+  const handleFormatChange = (_event: React.MouseEvent<HTMLElement>, updatedFormats: string[]) =>{
+    setFormats(updatedFormats)
+  }
+
   return (
     <Stack spacing={4}>
+
+              {/*Variant Button */}
       <Stack spacing={2} direction="row">
         <Button variant="text" href="https://google.com">
           texto
@@ -12,6 +30,7 @@ export const MuiButton = () => {
         <Button variant="outlined">Outlined</Button>
       </Stack>
 
+        {/*Color Button */}
       <Stack spacing={2} direction="row">
         <Button variant="contained" color="primary">
           Primary
@@ -36,6 +55,7 @@ export const MuiButton = () => {
         </Button>
       </Stack>
 
+        {/*Size Button */}
       <Stack display='block' spacing={2} direction='row'>
       <Button variant="contained" size="small">
           Small
@@ -63,6 +83,39 @@ export const MuiButton = () => {
         </IconButton>
 
       </Stack>
+
+        {/*Group Button */}
+      <Stack direction='row'>
+        <ButtonGroup variant="contained" orientation="vertical" size='large' color='secondary' aria-label='aligment button gruop'>
+        <Button onClick={()=> alert('Soy el left')} >
+          left
+        </Button>
+        <Button onClick={()=> alert('Soy el center')}>
+          center
+        </Button>
+        <Button onClick={()=> alert('Soy el right')}>
+          Right
+        </Button>
+
+
+        </ButtonGroup>
+  
+
+      </Stack>
+
+         {/*Toggle Button */}
+         <Stack direction='row'>
+        <ToggleButtonGroup  aria-label='text formatting' value={formats} onChange={handleFormatChange} size="medium" color="success" orientation="vertical" exclusive> 
+       
+          <ToggleButton value='bold' aria-label='bold'> <FormatBoldIcon></FormatBoldIcon></ToggleButton>
+          <ToggleButton value='Italic' aria-label='italic'> <FormatItalicIcon></FormatItalicIcon></ToggleButton>
+          <ToggleButton value='UnderLined' aria-label='underlined'> <FormatUnderLinedIcon></FormatUnderLinedIcon></ToggleButton>
+
+        </ToggleButtonGroup>
+  
+
+      </Stack>
+
     </Stack>
   );
 };
